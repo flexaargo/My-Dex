f36ab334c61cf9fd44ec1e8a236195302a697e53
Alex Fargo <me@alexfargo.xyz>
2019-04-23 14:24:54 -0700

    Removed storyboard. Created TestingController

---
+++ My Dex/Controllers/TestingController.swift
@@ -0,0 +1,13 @@
+//
+//  TestingController.swift
+//  My Dex
+//
+//  Created by Alex Fargo on 4/23/19.
+//  Copyright © 2019 Alex Fargo. All rights reserved.
+//
+
+import UIKit
+
+class TestingController: UITableViewController {
+  
+}
