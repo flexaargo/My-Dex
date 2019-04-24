0515a0981b4e339d0190df10d29b67176fb97508
Alex Fargo <me@alexfargo.xyz>
2019-04-24 02:05:53 -0700

    Created pokedex cell. Fetching data in collection view

--- My Dex/Controllers/PokedexCollectionController.swift
+++ My Dex/Controllers/PokedexCollectionController.swift
@@ -9,8 +9,46 @@
 import UIKit
 
 class PokedexCollectionController: BaseCollectionView {
+  var pokedexEntries = [Pokemon]()
   override func viewDidLoad() {
     super.viewDidLoad()
     navigationItem.title = "My Dex"
+    collectionView.backgroundColor = .white
+    collectionView.register(PokedexCell.self, forCellWithReuseIdentifier: PokedexCell.reuseIdentifier)
+    fetchPokemon()
+  }
+}
+
+private extension PokedexCollectionController {
+  func fetchPokemon() {
+    PokeApi.shared.fetchPokemon(toId: PokeApi.shared.maxPokemonId) { (pokemon) in
+      self.pokedexEntries = pokemon
+      self.fetchedPokemon()
+    }
+  }
+  
+  func fetchedPokemon() {
+    pokedexEntries.sort { (p1, p2) -> Bool in
+      return p1.id < p2.id
+    }
+    collectionView.reloadData()
+  }
+}
+
+extension PokedexCollectionController {
+  override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+    return pokedexEntries.count
+  }
+  
+  override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+    let cell = collectionView.dequeueReusableCell(withReuseIdentifier: PokedexCell.reuseIdentifier, for: indexPath) as! PokedexCell
+    cell.pokemon = pokedexEntries[indexPath.item]
+    return cell
+  }
+}
+
+extension PokedexCollectionController: UICollectionViewDelegateFlowLayout {
+  func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+    return .init(width: 100, height: 100)
   }
 }
---
+++ My Dex/Views/PokedexCell.swift
@@ -0,0 +1,27 @@
+//
+//  PokedexCell.swift
+//  My Dex
+//
+//  Created by Alex Fargo on 4/24/19.
+//  Copyright © 2019 Alex Fargo. All rights reserved.
+//
+
+import UIKit
+
+class PokedexCell: UICollectionViewCell {
+  var pokemon: Pokemon! {
+    didSet {
+      
+    }
+  }
+  
+  override init(frame: CGRect) {
+    super.init(frame: frame)
+  }
+  
+  required init?(coder aDecoder: NSCoder) {
+    fatalError("init(coder:) has not been implemented")
+  }
+}
+
+extension PokedexCell: ReuseIdentifying {}
